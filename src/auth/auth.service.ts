3004f3c99fde74a9b26db18553fc42a20f62ede5
Иван Гнедько <ignedko34@gmail.com>
2023-07-27 17:53:42 +0300

    refresh token refactoring

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -47,14 +47,8 @@ export class AuthService {
     }
 
     async refresh(refreshToken: string, agent: string): Promise<Tokens> {
-        const token = await this.prismaService.token.findUnique({ where: { token: refreshToken } });
-        if (!token) {
-            throw new UnauthorizedException();
-        }
-        await this.prismaService.token.delete({
-            where: { token: refreshToken },
-        });
-        if (new Date(token.exp) < new Date()) {
+        const token = await this.prismaService.token.delete({ where: { token: refreshToken } });
+        if (!token || new Date(token.exp) < new Date()) {
             throw new UnauthorizedException();
         }
         const user = await this.userService.findOne(token.userId);
